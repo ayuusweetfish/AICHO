7bb883b8977238900be7dd010868952fe953f77b
Ayu <claire@ayu.land>
2024-06-03 17:29:26 +0800

    Add I2S audio output through PIO

--- fw/main.c
+++ fw/main.c
@@ -1,6 +1,9 @@
 #include "pico/stdlib.h"
+#include "hardware/clocks.h"
 #include <stdarg.h>
 #include <stdio.h>
+
+#include "i2s.pio.h"
 
 static inline void my_putc(uint8_t c)
 {
@@ -22,13 +25,38 @@ static void my_printf(const char *restrict fmt, ...)
 
 int main()
 {
-  uart_init(uart0, 115200);
-  gpio_set_function(16, GPIO_FUNC_UART);
-  gpio_set_function(17, GPIO_FUNC_UART);
+  // 132 MHz
+  // python pico-sdk/src/rp2_common/hardware_clocks/scripts/vcocalc.py 132
+  set_sys_clock_pll(1584000000, 6, 2);
 
   const uint32_t LED_PIN = 25;
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  uart_init(uart0, 115200);
+  gpio_set_function(16, GPIO_FUNC_UART);
+  gpio_set_function(17, GPIO_FUNC_UART);
+
+  my_printf("sys clk %u\n", clock_get_hz(clk_sys));
+
+  uint offset = pio_add_program(pio0, &i2s_program);
+  uint sm = pio_claim_unused_sm(pio0, true);
+  i2s_program_init(pio0, sm, offset, 6, 7);
+
+  //sm_config_set_clkdiv(
+  pio_sm_set_enabled(pio0, sm, true);
+
+  uint32_t count = 0;
+
+  while (1) {
+    pio_sm_put_blocking(pio0, sm, 0xccccd4d4u);
+    if (++count == 48000) {
+      static int parity = 0;
+      gpio_put(LED_PIN, parity ^= 1);
+      my_printf("second!\n");
+      count = 0;
+    }
+  }
 
   while (1) {
     gpio_put(LED_PIN, 1); sleep_ms(100);
