59f9f0d205f9fbd889272f476b9cc638c038c009
Ayu <claire@ayu.land>
2024-06-05 14:05:49 +0800

    Pause and resume DMA transfers

--- fw/main.c
+++ fw/main.c
@@ -28,7 +28,8 @@ int my_printf(const char *restrict fmt, ...)
   return 0;
 }
 
-static int16_t audio_buf[9600];
+static int16_t audio_buf[2400];
+static const uint32_t audio_buf_half_size = (sizeof audio_buf) / (sizeof audio_buf[0]) / 2;
 
 static inline void refill_buffer(int half);
 static void dma_irq0_handler();
@@ -67,16 +68,12 @@ int main()
 
   pio_sm_set_enabled(pio0, sm, true);
 
-#define GAIN 0.5  // 16 for 4Ω speaker, 1 for headphone
+#define GAIN 8  // 16 for 4Ω speaker, 1 for headphone
 
-  for (int i = 0; i < 9600; i++) {
-    int16_t sample = (i < 4800 ?
-      (int16_t)(0.5f + GAIN * 256 * sin((float)(i % 100) / 100 * (float)M_PI * 2)) :
-      (int16_t)(0.5f + GAIN * 256 * sin((float)(i %  50) /  50 * (float)M_PI * 2)));
+  for (int i = 0; i < audio_buf_half_size * 2; i++) {
+    int16_t sample = (int16_t)(0.5f + GAIN * 256 * sin((float)(i % 100) / 100 * (float)M_PI * 2));
     audio_buf[i] = sample;
   }
-
-  const uint32_t half_size = (sizeof audio_buf) / (sizeof audio_buf[0]) / 2;
 
   dma_channel_config dma_ch0 = dma_channel_get_default_config(0);
   channel_config_set_read_increment(&dma_ch0, true);
@@ -85,7 +82,7 @@ int main()
   channel_config_set_dreq(&dma_ch0, pio_get_dreq(pio0, sm, /* is_tx */ true));
   channel_config_set_chain_to(&dma_ch0, 2);
   dma_channel_set_irq0_enabled(0, true);
-  dma_channel_configure(0, &dma_ch0, &pio0->txf[sm], audio_buf, half_size, false);
+  dma_channel_configure(0, &dma_ch0, &pio0->txf[sm], audio_buf, audio_buf_half_size, false);
 
   dma_channel_config dma_ch1 = dma_channel_get_default_config(1);
   channel_config_set_read_increment(&dma_ch1, true);
@@ -94,13 +91,13 @@ int main()
   channel_config_set_dreq(&dma_ch1, pio_get_dreq(pio0, sm, /* is_tx */ true));
   channel_config_set_chain_to(&dma_ch1, 3);
   dma_channel_set_irq0_enabled(1, true);
-  dma_channel_configure(1, &dma_ch1, &pio0->txf[sm], audio_buf + half_size, half_size, false);
+  dma_channel_configure(1, &dma_ch1, &pio0->txf[sm], audio_buf + audio_buf_half_size, audio_buf_half_size, false);
 
   irq_set_exclusive_handler(11, dma_irq0_handler);
   irq_set_enabled(11, true);
 
   static uint32_t addr0 = (uint32_t)&audio_buf[0];
-  static uint32_t addr1 = (uint32_t)&audio_buf[half_size];
+  static uint32_t addr1 = (uint32_t)&audio_buf[audio_buf_half_size];
 
   dma_channel_config dma_ch2 = dma_channel_get_default_config(2);
   channel_config_set_read_increment(&dma_ch2, false);
@@ -121,18 +118,29 @@ int main()
     static uint32_t i = 0;
     i++;
     my_printf("Hello, UART %u!%c", i, i % 2 == 0 ? '\n' : '\t');
+    if (i >= 5 && i % 5 == 0) {
+      channel_config_set_enable(&dma_ch0, false);
+      dma_channel_set_config(0, &dma_ch0, false);
+    } else if (i >= 5 && i % 5 == 1) {
+      channel_config_set_enable(&dma_ch0, true);
+      dma_channel_configure(0, &dma_ch0, &pio0->txf[sm], audio_buf, audio_buf_half_size, true);
+    }
   }
 }
 
-void fill_buffer(int half)
+void fill_buffer(bool half)
 {
+  static uint32_t count = 0;
   static uint32_t seed = 1;
-  seed = seed * 1103515245 + 12345;
+  if (++count == 4) {
+    count = 0;
+    seed = seed * 1103515245 + 12345;
+  }
   uint32_t period = ((seed >> 3) ^ (seed >> 9)) % 50 + 50;
-  for (int i = 0; i < 4800; i++) {
+  for (int i = 0; i < audio_buf_half_size; i++) {
     int16_t sample =
-      (int16_t)(0.5f + GAIN * 256 * sin((float)i / period * (float)M_PI * 2));
-    audio_buf[4800 * half + i] = sample;
+      (int16_t)(0.5f + GAIN * 256 * sin((float)(i + count * audio_buf_half_size) / period * (float)M_PI * 2));
+    audio_buf[audio_buf_half_size * (int)half + i] = sample;
   }
 }
 
@@ -140,11 +148,9 @@ void dma_irq0_handler()
 {
   if (dma_channel_get_irq0_status(0)) {
     dma_channel_acknowledge_irq0(0);
-    gpio_put(23, 0);
     fill_buffer(0);
   } else {
     dma_channel_acknowledge_irq0(1);
-    gpio_put(23, 1);
     fill_buffer(1);
   }
 }
