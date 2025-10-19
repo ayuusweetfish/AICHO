1114a2c1186e877b6fd1cdf58552053b8bee568d
Ayu <claire@ayu.land>
2025-10-19 01:06:42 -0400

    central_fw: Read key events

--- central_fw/keyboard.c
+++ central_fw/keyboard.c
@@ -1,4 +1,5 @@
 #include <dirent.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <linux/input.h>
 #include <stdbool.h>
@@ -19,10 +20,44 @@ bool is_keyboard(const char *device)
   return (ev & (1 << EV_KEY));
 }
 
+void monitor_keyboard(const char *device)
+{
+  int fd = open(device, O_RDONLY | O_NONBLOCK);
+  if (fd == -1) {
+    perror("open");
+    exit(1);
+  }
+
+  printf("Monitoring keyboard %s\n", device);
+
+  struct input_event ev;
+
+  while (1) {
+    ssize_t n = read(fd, &ev, sizeof ev);
+    if (n == -1) {
+      if (errno == EAGAIN) {
+        usleep(100000);
+        continue;
+      } else {
+        perror("read");
+        exit(1);
+      }
+    }
+    if (n == sizeof ev && ev.type == EV_KEY && ev.value == 1) {
+      printf("+%d %s\n", ev.code, device);
+    }
+  }
+
+  close(fd);
+}
+
 int main()
 {
   DIR *dir = opendir("/dev/input");
-  if (dir == NULL) perror("opendir");
+  if (dir == NULL) {
+    perror("opendir");
+    exit(1);
+  }
 
   struct dirent *ent;
   while ((ent = readdir(dir)) != NULL) {
@@ -30,6 +65,7 @@ int main()
       char full_path[64] = "/dev/input/";
       strncat(full_path, ent->d_name, (sizeof full_path) - 1);
       printf("%s %d\n", full_path, (int)is_keyboard(full_path));
+      monitor_keyboard("/dev/input/event2");
     }
   }
 
