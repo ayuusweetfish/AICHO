43178bfd5ac09a825997d1995cd6eb88cf46bebd
Ayu <claire@ayu.land>
2025-10-19 01:11:04 -0400

    central_fw: Monitor all keyboards

--- central_fw/keyboard.c
+++ central_fw/keyboard.c
@@ -20,35 +20,30 @@ bool is_keyboard(const char *device)
   return (ev & (1 << EV_KEY));
 }
 
-void monitor_keyboard(const char *device)
+void monitor_keyboard(const int *fds, int n_fds)
 {
-  int fd = open(device, O_RDONLY | O_NONBLOCK);
-  if (fd == -1) {
-    perror("open");
-    exit(1);
-  }
-
-  printf("Monitoring keyboard %s\n", device);
-
   struct input_event ev;
 
   while (1) {
-    ssize_t n = read(fd, &ev, sizeof ev);
-    if (n == -1) {
-      if (errno == EAGAIN) {
-        usleep(100000);
-        continue;
-      } else {
-        perror("read");
-        exit(1);
+    bool again = false;
+    // XXX: We can use poll()
+    for (int i = 0; i < n_fds; i++) {
+      ssize_t n = read(fds[i], &ev, sizeof ev);
+      if (n == -1) {
+        if (errno == EAGAIN) {
+          again = true;
+          continue;
+        } else {
+          perror("read");
+          exit(1);
+        }
+      }
+      if (n == sizeof ev && ev.type == EV_KEY && ev.value == 1) {
+        printf("+%d %d\n", fds[i], ev.code);
       }
     }
-    if (n == sizeof ev && ev.type == EV_KEY && ev.value == 1) {
-      printf("+%d %s\n", ev.code, device);
-    }
+    if (again) usleep(100000);
   }
-
-  close(fd);
 }
 
 int main()
@@ -60,15 +55,28 @@ int main()
   }
 
   struct dirent *ent;
+  int fds[64], n_fds = 0;
+
   while ((ent = readdir(dir)) != NULL) {
     if (strncmp(ent->d_name, "event", 5) == 0) {
       char full_path[64] = "/dev/input/";
       strncat(full_path, ent->d_name, (sizeof full_path) - 1);
-      printf("%s %d\n", full_path, (int)is_keyboard(full_path));
-      monitor_keyboard("/dev/input/event2");
+      if (is_keyboard(full_path)) {
+        int fd = open(full_path, O_RDONLY | O_NONBLOCK);
+        if (fd == -1) {
+          perror("open");
+          exit(1);
+        }
+        printf("Monitoring keyboard %s (%d)\n", full_path, fd);
+        if (n_fds < sizeof fds / sizeof(int))
+          fds[n_fds++] = fd;
+      }
     }
   }
 
   closedir(dir);
+
+  monitor_keyboard(fds, n_fds);
+
   return 0;
 }
