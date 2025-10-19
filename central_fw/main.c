20cff743f5eea60af5aee2c43230d8b7995f27b2
Ayu <claire@ayu.land>
2025-10-19 01:36:36 -0400

    central_fw: Add serial

--- central_fw/main.c
+++ central_fw/main.c
@@ -1,9 +1,11 @@
-// cc main.c keyboard.c /dev/shm/a.out && /dev/shm/a.out
+// cc main.c keyboard.c serial.c /dev/shm/a.out && /dev/shm/a.out
 
 #include <stdio.h>
 #include <unistd.h>
 
 void keyboard_start(void (*callback)(int));
+void serial_start(const char *devices[], int n_devices);
+void serial_tx(int index, const void *buf, int len);
 
 void keyboard_callback(int n)
 {
@@ -13,6 +15,13 @@ void keyboard_callback(int n)
 int main()
 {
   keyboard_start(keyboard_callback);
+
+  serial_start((const char *[4]){
+    "/dev/ttyS2",
+    "/dev/ttyS1",
+    "/dev/ttyS5",
+    "/dev/ttyS3",
+  }, 4);
 
   while (1) usleep(1000000), puts("!");
   return 0;
